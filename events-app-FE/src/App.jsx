d91a01b40be6e9c144716996ca850b244b379070
Arron-B <arronmail@yahoo.com>
2024-06-06 15:47:30 +0100

    react router set up
    front-end authentication working with redirects

--- events-app-FE/src/App.jsx
+++ events-app-FE/src/App.jsx
@@ -1,8 +1,21 @@
-import { useState } from "react";
+import { useState, createContext } from "react";
 import "./App.css";
+import { Outlet } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
-	return <></>;
+	const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
+
+	if (!isAuthenticated && !isLoading) {
+		loginWithRedirect();
+	}
+
+	return (
+		<>
+			<h1 className="mb-10">Event App</h1>
+			<Outlet />
+		</>
+	);
 }
 
 export default App;
--- events-app-FE/src/main.jsx
+++ events-app-FE/src/main.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import "./index.css";
+import ErrorPage from "./error-page";
+import Calendar from "./components/Calendar.jsx";
+import LoginButton from "./components/LoginButton.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: "login",
+				element: <LoginButton />,
+			},
+
+			{
+				path: "calendar/:calendarId",
+				element: <Calendar />,
+			},
+		],
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<Auth0Provider
+			domain="dev-02batacgo02k8aak.us.auth0.com"
+			clientId="rro2yWhpij2wYtYHBTkzmmVHcGyJ0RIV"
+			authorizationParams={{
+				redirect_uri: "http://localhost:5173/calendar/1",
+			}}
+		>
+			<RouterProvider router={router} />
+		</Auth0Provider>
+	</React.StrictMode>
+);
