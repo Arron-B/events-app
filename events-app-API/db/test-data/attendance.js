61a23144c4bd987ad213f1d2755683c178053c67
Arron-B <arronmail@yahoo.com>
2024-06-08 22:45:26 +0100

    test data edited slightly

--- events-app-API/db/test-data/attendance.js
+++ events-app-API/db/test-data/attendance.js
@@ -1,122 +1,138 @@
 module.exports = [
 	{
-		user_id: "auth0Id4",
-		event_id: 8,
+		user_id: "auth0Id16",
+		event_id: 3,
 	},
 	{
-		user_id: "auth0Id3",
-		event_id: 5,
+		user_id: "auth0Id14",
+		event_id: 2,
+	},
+	{
+		user_id: "auth0Id19",
+		event_id: 9,
 	},
 	{
 		user_id: "auth0Id12",
-		event_id: 10,
+		event_id: 6,
 	},
 	{
-		user_id: "auth0Id18",
-		event_id: 12,
-	},
-	{
-		user_id: "auth0Id20",
-		event_id: 3,
+		user_id: "auth0Id21",
+		event_id: 9,
 	},
 	{
 		user_id: "auth0Id1",
 		event_id: 7,
 	},
 	{
-		user_id: "auth0Id5",
-		event_id: 14,
-	},
-	{
-		user_id: "auth0Id6",
-		event_id: 2,
-	},
-	{
-		user_id: "auth0Id7",
-		event_id: 11,
-	},
-	{
-		user_id: "auth0Id8",
+		user_id: "auth0Id20",
 		event_id: 1,
 	},
 	{
-		user_id: "auth0Id9",
+		user_id: "auth0Id22",
 		event_id: 13,
+	},
+	{
+		user_id: "auth0Id25",
+		event_id: 7,
+	},
+	{
+		user_id: "auth0Id3",
+		event_id: 2,
+	},
+	{
+		user_id: "auth0Id4",
+		event_id: 3,
+	},
+	{
+		user_id: "auth0Id23",
+		event_id: 14,
+	},
+	{
+		user_id: "auth0Id19",
+		event_id: 4,
+	},
+	{
+		user_id: "auth0Id25",
+		event_id: 1,
+	},
+	{
+		user_id: "auth0Id1",
+		event_id: 10,
+	},
+	{
+		user_id: "auth0Id15",
+		event_id: 6,
 	},
 	{
 		user_id: "auth0Id10",
 		event_id: 4,
 	},
 	{
-		user_id: "auth0Id11",
-		event_id: 9,
-	},
-	{
-		user_id: "auth0Id13",
-		event_id: 15,
-	},
-	{
-		user_id: "auth0Id14",
-		event_id: 6,
-	},
-	{
-		user_id: "auth0Id15",
-		event_id: 3,
-	},
-	{
-		user_id: "auth0Id16",
-		event_id: 5,
-	},
-	{
-		user_id: "auth0Id17",
-		event_id: 8,
-	},
-	{
-		user_id: "auth0Id18",
-		event_id: 2,
-	},
-	{
-		user_id: "auth0Id19",
+		user_id: "auth0Id4",
 		event_id: 12,
 	},
 	{
-		user_id: "auth0Id20",
-		event_id: 11,
+		user_id: "auth0Id25",
+		event_id: 8,
 	},
 	{
-		user_id: "auth0Id21",
-		event_id: 1,
+		user_id: "auth0Id20",
+		event_id: 14,
+	},
+	{
+		user_id: "auth0Id16",
+		event_id: 9,
+	},
+	{
+		user_id: "auth0Id5",
+		event_id: 15,
+	},
+	{
+		user_id: "auth0Id25",
+		event_id: 3,
 	},
 	{
 		user_id: "auth0Id22",
+		event_id: 12,
+	},
+	{
+		user_id: "auth0Id12",
+		event_id: 4,
+	},
+	{
+		user_id: "auth0Id13",
+		event_id: 10,
+	},
+	{
+		user_id: "auth0Id2",
+		event_id: 14,
+	},
+	{
+		user_id: "auth0Id21",
 		event_id: 10,
 	},
 	{
 		user_id: "auth0Id23",
-		event_id: 4,
+		event_id: 5,
 	},
 	{
-		user_id: "auth0Id24",
+		user_id: "auth0Id15",
+		event_id: 10,
+	},
+	{
+		user_id: "auth0Id1",
 		event_id: 13,
 	},
 	{
-		user_id: "auth0Id25",
+		user_id: "auth0Id1",
+		event_id: 1,
+	},
+	{
+		user_id: "auth0Id1",
 		event_id: 6,
 	},
 	{
 		user_id: "auth0Id1",
-		event_id: 9,
-	},
-	{
-		user_id: "auth0Id2",
-		event_id: 7,
-	},
-	{
-		user_id: "auth0Id3",
 		event_id: 15,
-	},
-	{
-		user_id: "auth0Id4",
-		event_id: 14,
 	},
 ];
